c53c75e7c7ee0345dc8b13f85ba3a998861f0b7a
Corinna Brock Moore <corinna.brock@gmail.com>
2022-11-22 13:56:07 -0500

    pseudo code in, working on readData function and main data function

--- math_game/math_game.go
+++ math_game/math_game.go
@@ -1,5 +1,56 @@
 package mathgame
 
+import (
+	"encoding/csv"
+	"os"
+)
+
+type Question struct {
+	q1  int
+	q2  int
+	ans int
+}
+
 func main() {
+	// read data here
+	// log fatal errors
+	// create each question, figure out delimiters
+	// print out questions just as a test, then move on
+}
+
+func DataIn(fileName string) ([][]string, error) {
+	file, err := os.Open(fileName)
+
+	if err != nil {
+		return [][]string{}, err
+	}
+
+	defer file.Close()
+
+	r := csv.NewReader(file)
+
+	records, err := r.ReadAll()
+	if err != nil {
+		return [][]string{}, err
+	}
+
+	return records, nil
 
 }
+
+// parse in csv
+// separate into questions
+// output to user
+// take in user info
+// check against answer
+// track correct answers
+// track number of questions
+// at the end of the file, output correct/total questions
+
+// part 2:
+// add timer
+// default to 30 seconds
+// take in optional flag
+// clean white space around input
+// check for non-valid input (letters, etc)
+// shuffle questions
